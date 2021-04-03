ad21df99e666d7085bbb1c31202f5099145dd4f2
avestoinn <avesto.inn@gmail.com>
2021-04-04 03:18:23 +0500

    Инициализация

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/bekhruzdilshod/bank
+
+go 1.16
---
+++ pkg/types/types.go
@@ -0,0 +1,33 @@
+package types 
+
+type Money int64
+
+type Currency string
+
+const (
+	TJS Currency = "TJS"
+	RUB Currency = "RUB"
+	USD Currency = "USD"
+	EUR Currency = "EUR"
+)
+
+type Category string
+
+type PAN string
+
+type Card struct {
+	ID int 
+	PAN PAN 
+	Balance Money
+	Currency Currency
+	Color string
+	Name string
+	Active bool
+}
+
+type Payment struct {
+	ID int
+	Amount Money
+	Category Category
+}
+
